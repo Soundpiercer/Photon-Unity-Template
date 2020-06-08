dcff2335656e22cf9086393afd9c1f9eeefa0f24
Sound piercer Official <soundpiercer@gmail.com>
2020-06-08 23:00:12 +0900

    Add Unity-chan! Asset Package

---
+++ Photon Unity Template/Assets/unity-chan!/Unity-chan! Model/SplashScreen/Scripts/SplashScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+namespace UnityChan
+{
+	[ExecuteInEditMode]
+	public class SplashScreen : MonoBehaviour
+	{
+		void NextLevel ()
+		{
+            // Application.LoadLevel(Application.loadedLevel + 1); Deprecated
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+	}
+}
